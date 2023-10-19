f50152c43a4254e301c4a46d99ffb3b87d4f76c5
Eduardo <aguacatevolador1@gmail.com>
2023-10-19 12:13:02 -0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(CatalogApp());
+  runApp(MyApp());
 }
 
-class CatalogApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Catálogo de Películas',
-      home: CatalogScreen(),
+      title: 'Flutter Example App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Inicio de Sesión')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Nombre de Usuario'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Contraseña'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Aquí debes realizar la lógica de verificación de las credenciales.
+                // Si son válidas, navega a la página principal.
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => CatalogScreen()),
+                );
+              },
+              child: Text('Iniciar Sesión'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
